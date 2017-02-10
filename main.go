b5ed249e871cfa8e56c202ef37ebe4b2651a7d86
LLLeon <jiachenhui1989@gmail.com>
2017-02-10 17:00:11 +0800

    添加切换 token 功能 #4

--- main.go
+++ main.go
@@ -20,5 +20,5 @@ func initBeforeRun() {
 func main() {
 	initBeforeRun()
 
-	spider.GetRepos()
+	spider.GetReposByAPI()
 }
--- spider/spider.go
+++ spider/spider.go
@@ -1,6 +1,7 @@
 package spider
 
 import (
+	"errors"
 	"log"
 
 	"github.com/google/go-github/github"
@@ -38,15 +39,46 @@ func PrepareRepos() {
 	SpiderService = &SpiderServiceProvider{}
 }
 
-func GetRepos() {
+// 切换 token
+func changeToken() func() *github.Client {
+	var client *github.Client
 
-	// 添加身份验证, 提高请求速率
-	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: "54f7488c8f72d3e63692b2bf04167d97e7a29e1d"},
-	)
-	tc := oauth2.NewClient(oauth2.NoContext, ts)
+	i := 0
+	tokens := []string{
+		"54f7488c8f72d3e63692b2bf04167d97e7a29e1d",
+		"5511599ff7aebf94476ce3eda7741ab7ae797ef9",
+		"78d1dcb42b8c4368884603cfcd4f3a1581d771d2",
+	}
 
-	client := github.NewClient(tc)
+	return func() *github.Client {
+		if i < len(tokens) {
+			ts := oauth2.StaticTokenSource(
+				&oauth2.Token{AccessToken: tokens[i]},
+			)
+			tc := oauth2.NewClient(oauth2.NoContext, ts)
+			client = github.NewClient(tc)
+
+			i += 1
+
+			return client
+		} else {
+
+			return nil
+		}
+	}
+}
+
+// 调用 GitHub 官方 API 获取库信息
+func GetReposByAPI() {
+	var client *github.Client
+
+	// 循环设置 token, 提高请求速率
+	nextClient := changeToken()
+	client = nextClient()
+
+	if client == nil {
+		errors.New("Tokens has run out.")
+	}
 
 	// 获取所有解析 csv 文件后所得的 id 记录
 	results, err := CsvService.GetAllRecords()
@@ -58,17 +90,24 @@ func GetRepos() {
 	for _, result := range results {
 		repo, _, err := client.Repositories.GetByID(result.ReposID)
 
+		// 遇到速率限制错误后, 切换 token
 		if _, ok := err.(*github.RateLimitError); ok {
 			log.Println("Hit rate limit.")
+			client = nextClient()
+
+			if client == nil {
+				errors.New("Tokens has run out.")
+			}
+
+			log.Println("Change token, get this repository again.")
+			repo, _, err = client.Repositories.GetByID(result.ReposID)
 		}
 
-		// 如果获取库时报错, 则跳过该库
 		if err != nil {
 			log.Print(err)
 			continue
 		}
 
-		// 如果是 fork 的库则跳过
 		if *repo.Fork == true {
 			log.Println("This repository is forked.")
 
